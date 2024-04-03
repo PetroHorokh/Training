080ca5b1e14ba184c2e46ecf7cfd1bfeaf9a71ee
Normal_Pete <petrohorokh@gmail.com>
2024-04-03 14:44:40 +0300

    ADO.NET service creation
    Creation of project for using ADO.NET as a collection of services. Implementing both connected and disconnected architecture approaches. Created several DTOs to represent results

---
+++ Rent.ADO.NET/DTOs/AddressDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Rent.ADO.NET.DTOs;
+
+public class AddressDto
+{
+    public Guid AddressId { get; set; }
+
+    public string City { get; set; } = null!;
+
+    public string Street { get; set; } = null!;
+
+    public string Building { get; set; } = null!;
+
+    public override string ToString() => $"\nAddress with id {AddressId} information\nCity: {City}\nStreet: {Street}\nBuilding: {Building}";
+}
---
+++ Rent.ADO.NET/Services/Contracts/IConnectedArchitecture.cs
@@ -0,0 +1,10 @@
+﻿using Rent.ADO.NET.DTOs;
+
+namespace Rent.ADO.NET.Services.Contracts;
+
+public interface IConnectedArchitecture
+{
+    Task<IEnumerable<AddressDto>> GetAllAddressesAsync();
+
+    Task<AddressDto?> GetAddressByIdAsync(Guid addressId);
+}
